8e684617774064b931b0279bf8e7b2a0bf94ab86
Sergei Kovalchuk (ArSysOp) <sergei.kovalchuk@arsysop.ru>
2022-11-10 16:11:56 +0300

    The number of circles added for estimation

--- src/main/java/water/pool/core/WaterPoolApplication.java
+++ src/main/java/water/pool/core/WaterPoolApplication.java
@@ -16,14 +16,14 @@ public class WaterPoolApplication {
 	static Logger logger = Logger.getLogger(WaterPoolApplication.class.getName());
 
 	public static void main(String[] args) {
-		int[] landscape = { 5, 2, 3, 4, 5, 4, 0, 3, 1 ,4};
+		int[] landscape = { 5, 2, 3, 4, 5, 4, 0, 3, 1, 4 };
 		if (!WaterPoolCalculator.validateLandscape(landscape)) {
 			logger.log(Level.INFO, "Source landscape data unvalid... ");
 
 		} else {
-			logger.log(Level.INFO, "Total square [approach -1] : {0} ", //
+			logger.log(Level.INFO, "Total square [approach 1] : {0} ", //
 					WaterPoolCalculator.calculateWaterAmountHorizontallyandVertically(landscape));
-			logger.log(Level.INFO, "Total square [approach -2] : {0} ", //
+			logger.log(Level.INFO, "Total square [approach 2] : {0} ", //
 					WaterPoolCalculator.calcFromRightAndLeft(landscape));
 		}
 	}
